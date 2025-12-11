19c76354ed122abdea06e33ec149e94dbbca14a2
Samuel Gomez <samuelgomez4010@gmail.com>
2025-12-11 14:23:11 -0500

    flt_stock_extended: Rename field label from "Cantidad Conos" to "Conos" and adjust xpath for field placement in stock.move.line view

--- flt_stock_extended/models/stock_move_line.py
+++ flt_stock_extended/models/stock_move_line.py
@@ -4,4 +4,4 @@ from odoo import models, fields
 
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
-    cantidad_conos = fields.Integer(string="Cantidad Conos")
+    cantidad_conos = fields.Integer(string="Conos")
