8be491556f1ea2426bd495c7c2405c2214f70f42
Samuel Gomez <samuelgomez4010@gmail.com>
2025-12-11 13:38:42 -0500

    flt_stock_extended: Initialize stock extended module and add stock move line enhancements

---
+++ flt_stock_extended/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Stock Extended',
+    'category': 'Stock',
+    'summary': 'Add new features to stock module',
+    'depends': ['stock'],
+    'data': [
+        'views/stock_picking_views',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ flt_stock_extended/models/stock_move_line.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields
+
+
+class StockMoveLine(models.Model):
+    _inherit = 'stock.move.line'
+    cantidad_conos = fields.Integer(string="Cantidad Conos")
