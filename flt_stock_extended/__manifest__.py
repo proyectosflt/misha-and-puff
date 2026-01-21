6ac6092960a20e10aaf872fc57b5b95b4fe859dd
Samuel Gomez <samuelgomez4010@gmail.com>
2026-01-21 13:26:27 -0500

    flt_stock_extended: Add report package barcode template to enhance product packaging information

--- flt_stock_extended/__manifest__.py
+++ flt_stock_extended/__manifest__.py
@@ -8,6 +8,7 @@
         'security/ir.model.access.csv',
         'views/stock_move_line.xml',
         'views/product_product.xml',
+        'views/report_package_barcode.xml',
         'views/stock_quant.xml',
         'views/stock_quant_package.xml',
     ],
