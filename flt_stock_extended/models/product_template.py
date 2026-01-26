166172e2f13bb8383cc31e4e80796480bac4e851
Samuel Gomez <samuelgomez4010@gmail.com>
2026-01-26 12:50:39 -0500

    flt_stock_extended: Implement weight defaulting for product creation based on UoM

--- flt_stock_extended/models/product_template.py
+++ flt_stock_extended/models/product_template.py
@@ -12,7 +12,17 @@ class ProductTemplate(models.Model):
                 raise ValidationError("El peso del producto no puede ser 0.")
         return res
 
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if 'uom_id' in vals:
+                uom = self.env['uom.uom'].browse(vals['uom_id'])
+                if uom.name == 'kg' and vals.get('weight', 0) == 0:
+                    vals['weight'] = 1.0
+        return super(ProductTemplate, self).create(vals_list)
+
     @api.onchange('uom_id')
+
     def _onchange_uom_id_weight(self):
         if self.uom_id.name == 'kg' and self.weight == 0:
             self.weight = 1.0
