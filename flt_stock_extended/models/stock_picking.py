d213a1376d1c7fe0028f0db01866a0e1bb9d38c0
Samuel Gomez <samuelgomez4010@gmail.com>
2026-02-18 12:28:04 -0500

    flt_stock_extended: Add 'Número guía de remisión' field to stock picking model and update view to include it

--- flt_stock_extended/models/stock_picking.py
+++ flt_stock_extended/models/stock_picking.py
@@ -4,4 +4,8 @@ from odoo import models, fields
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
-    pass
+    
+    numero_guia_remision = fields.Char(
+        string='Número guía de remisión',
+        help='Número de la guía de remisión'
+    )
