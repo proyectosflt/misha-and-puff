24a5855e40b94748739fac43a49de148c7e219b6
Samuel Gomez <samuelgomez4010@gmail.com>
2026-09-23 11:13:05 -0400

    stock_no_negative: add functionality to manage negative stock levels for products, categories, and locations; implement constraints to prevent negative stock validation; create tests for stock operations; update documentation and views accordingly.

--- flt_stock_extended/models/stock_move_line.py
+++ flt_stock_extended/models/stock_move_line.py
@@ -9,7 +9,7 @@ class StockMoveLine(models.Model):
     cono_id = fields.Many2one('tipo.cono', string='Tipo de Cono')
     tara_bolsa = fields.Float(string="Tara bolsa", compute='_compute_tara_bolsa', store=True, readonly=False, digits='Stock Weight')
     tara_cono = fields.Float(string="Tara cono unitaria", compute='_compute_tara_cono', store=True, readonly=False, digits='Stock Weight')
-    tara_cono_total = fields.Float(string="Tara cono total", compute='_compute_tara_cono', store=True, readonly=False, digits='Stock Weight')
+    tara_cono_total = fields.Float(string="Tara cono total", compute='_compute_tara_cono_total', store=True, readonly=False, digits='Stock Weight')
     peso_bruto = fields.Float(string="Peso bruto", digits='Stock Weight')
     peso_neto = fields.Float(string="Peso neto", compute='_compute_peso_neto', store=True, readonly=False, digits='Stock Weight')
 
@@ -31,7 +31,13 @@ class StockMoveLine(models.Model):
                 continue
             if not record.tara_cono and record.cono_id:
                 record.tara_cono = record.cono_id.tara_cono or 0.0
+
+    @api.depends('tara_cono', 'cantidad_conos')
+    def _compute_tara_cono_total(self):
+        for record in self:
             record.tara_cono_total = (record.tara_cono or 0.0) * (record.cantidad_conos or 0)
+            
+    
 
     @api.depends('peso_bruto', 'tara_bolsa', 'tara_cono', 'cantidad_conos')
     def _compute_peso_neto(self):
